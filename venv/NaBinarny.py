d6db8f41f0f539af19180669b04c7d0774809698
Agnieszka <agulab7@interia.pl>
2019-04-25 10:33:12 +0200

    dodano listę t w celu sprawdzenia zapisu

--- venv/NaBinarny.py
+++ venv/NaBinarny.py
@@ -3,5 +3,10 @@ def convert_to_bin (digit):
     while digit>0:
         w = [digit % 2] + w  # % - reszta z dzielenia
         digit //= 2  #część całkowita
+        t = [];
+        t = [5] + t
+        t=[8] + t
+    print(t)
     return w
+
 print("liczba binarna 125=",convert_to_bin(125))
